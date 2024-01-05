36644b3ba21309aeba06b3bdd1ea1f0a40807f6d
Guilherme Rissi <102769917+glrmrissi@users.noreply.github.com>
2024-01-05 20:54:43 -0300

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,5 @@
 let count = 1;
+const totalSlides = 8;
 
 document.getElementById("radio1").checked = true;
 
@@ -7,10 +8,34 @@ setInterval(function() {
 }, 6000);
 
 function nextImg() {
-    count++;
-    if (count > 4) {
-        count = 1;
-    }
+  const radioSelecionado = document.querySelector('input[name="radio-btn"]:checked');
+
+  if (!radioSelecionado || parseInt(radioSelecionado.id.replace('radio', '')) === totalSlides) {
+      count = 1;
+  } else {
+      count = parseInt(radioSelecionado.id.replace('radio', '')) + 1;
+  }
+
 
     document.getElementById("radio" + count).checked = true;
+    updateLabelBackground();
 }
+
+function updateLabelBackground() {
+    var radioButtons = document.querySelectorAll('input[type="radio"]');
+    var manualLabels = document.querySelectorAll('.manual_btn');
+
+    radioButtons.forEach(function (radio, index) {
+      if (radio.checked) {
+        manualLabels[index].classList.add('checked');
+      } else {
+        manualLabels[index].classList.remove('checked');
+      }
+    });
+}
+
+updateLabelBackground();
+
+document.querySelectorAll('input[type="radio"]').forEach(function (radio) {
+    radio.addEventListener('change', updateLabelBackground);
+});
